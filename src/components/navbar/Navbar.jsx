94a13388cacf706e0831150a544e7f08b32f667f
Badar <86910185+badaruddinomar@users.noreply.github.com>
2023-12-16 17:44:33 +0600

    some bug solved02

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 // Menubar--
-import MenuBar from "./Menubar";
+import MenuBar from "./MenuBar";
 import "./navbar.scss";
 import { useRef, useState } from "react";
 import { useSelector } from "react-redux";
